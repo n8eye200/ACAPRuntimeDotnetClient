81c9e79326b4dbdbc5f0acca8428d19c2b01b28d
n8eye200 <nathan.schlehlein@axis.com>
2023-01-19 17:04:19 -0600

    A start of something.  Something beautiful?  Time will tell.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("http://192.168.245.134:5000");
+        var videoClient = new Videocapture.V1.VideoCapture.VideoCaptureClient(channel);
+
+        var newStreamRequest = new Videocapture.V1.NewStreamRequest();
+
+        newStreamRequest.Settings = new Videocapture.V1.StreamSettings();
+
+        newStreamRequest.Settings.Width = 1920;
+        newStreamRequest.Settings.Height = 1080;
+        newStreamRequest.Settings.Format = Videocapture.V1.StreamFormat.VdoFormatJpeg;
+
+        Console.WriteLine("Requested Video Stream Settings: " + newStreamRequest.ToString());
+
+        Videocapture.V1.NewStreamResponse videoChannel = await videoClient.NewStreamAsync(newStreamRequest);
+
+        Console.WriteLine(videoChannel.StreamId);
+
+    }
+}
